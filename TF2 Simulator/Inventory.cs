7b326a4e873fac207171030212acae7d9126e229
Partydonut214 <coolkidz124053@gmail.com>
2023-04-09 11:53:08 -0400

    EXPERIMENTAL: Added Early Versions of Save System. Done: Create new Saves based off of a Template. ToDo: Create an Inventory, and a way for the saves to be specifically read and overwriten.

---
+++ TF2 Simulator/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TF2_Simulator
+{
+    internal class Inventory
+    {
+        public static void InventoryLoader()
+        {
+            string Savepath = @"C:\Users\Public\Documents\TF2Sim\Save_01.txt";
+            foreach (char inventory in Savepath)
+            {
+                using (StreamReader sr = File.OpenText(Savepath))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        //Console.WriteLine(s);
+                        if (s.StartsWith("cID"))
+                        {
+                            Console.WriteLine($"{s}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
